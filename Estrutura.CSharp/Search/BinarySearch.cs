5bfbb8bdcfffda09865fdcac8fe799cbf9e141e9
Gabriel Faria <gabriel@negocieonline.com.br>
2020-12-23 18:27:05 -0300

    binary search

---
+++ Estrutura.CSharp/Search/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿namespace Estrutura.CSharp.Search
+{
+    public static class BinarySearch
+    {
+        public static bool HasNumber(this int[] array, int number)
+        {
+            bool found = false;
+            int start = 0;
+            int end = array.Length - 1;
+            int middle;
+            while (start <= end)
+            {
+                middle = (int)((start + end) / 2);
+                if (array[middle] == number)
+                {
+                    found = true;
+                    break;
+                }
+                else if (array[middle] < number)
+                {
+                    start = middle + 1;
+                }
+                else
+                {
+                    end = middle - 1;
+                }
+
+            }
+
+            return found;
+
+        }
+    }
+}
